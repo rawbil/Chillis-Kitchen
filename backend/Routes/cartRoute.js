ce1d10c01167d31543d6de5e11a4ab80fc622f86
Bil <bildadsimiyu6@gmail.com>
2024-08-24 13:31:13 +0300

    authentication

--- backend/Routes/cartRoute.js
+++ backend/Routes/cartRoute.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const route = express.Router();
 const userModel = require('../Models/userModel');
+const jwt = require('jsonwebtoken')
 
 //authMiddleware
-const authMiddleware = () => {
-    
+const authMiddleware = async(req, res, next) => {
+    const {token} = req.headers;
+    if(!token) {
+       return res.json({success: false, message: "Authorization failed"});
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.body.userId = decoded.userId;
+        next();
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Authorization failed"});
+    }
 }
 
 //POST API/CART/ADD

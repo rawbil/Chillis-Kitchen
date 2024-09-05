e43cd5ab2362294aa3ef4f411c1d434fbc439bd7
Bil <bildadsimiyu6@gmail.com>
2024-09-05 11:18:42 +0300

    dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+const PORT = process.env.PORT;
 const foodRoute = require('./Routes/foodRoute');
 const userRoute = require('./Routes/userRoute');
 const cartRoute = require("./Routes/cartRoute");

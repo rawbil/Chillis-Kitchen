bd988ee171407b52f3abfe1f5a2568013e4fca8a
Bil <bildadsimiyu6@gmail.com>
2024-10-29 16:25:43 +0300

    update cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODBURI)
 //END OF IMPORTS
 
 const corsOptions = {
-  origin: ["https://chilliskitchen-online.vercel.app"],
+  origin: ["http://localhost:5173","https://chilliskitchen-online.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "token"],
